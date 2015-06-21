8b83ad8a1ef287d4ceb4d9448bc1632f9e484f43
kkb <kkb1107@gmail.com>
2015-06-21 21:59:56 +0900

    use noun only

--- text_clf.py
+++ text_clf.py
@@ -12,27 +12,26 @@ import os
 in : '関数型言語では関数を第一級オブジェクトとして扱う。'
 out: ['関数', '型', '言語', 'で', 'は', '関数', 'を', '第', '一', '級', 'オブジェクト', 'として', '扱う', '。']
 """
-def make_term_list(text):
-    tagger = MeCab.Tagger("-Owakati")
-    term_list = tagger.parse(text).strip("\n").split(" ")
-    del term_list[-1]
-    return term_list
+def make_noun_list(text) :
+    tagger = MeCab.Tagger("mecabrc")
+    noun_list = filter(lambda n : "名詞" in n, tagger.parse(text).split("\n"))
+    return map(lambda n : n.split("\t")[0], noun_list)
 
-def make_text_list(directory):
-    return map(lambda n:open(os.path.join(directory, n)).read(), os.listdir(directory))
+def make_text_list(directory) :
+    return map(lambda n : open(os.path.join(directory, n)).read(), os.listdir(directory))
 
 learn_text_list = make_text_list("learn")
 test_text_list = make_text_list("test")
 
-vectorizer = TfidfVectorizer(analyzer=make_term_list, min_df=1)
+vectorizer = TfidfVectorizer(analyzer=make_noun_list, min_df=1)
 
 X = vectorizer.fit_transform(learn_text_list)
 _X = vectorizer.transform(test_text_list)
 y = np.array(["functional", "OOP"])
 
-#clf = MultinomialNB()
+clf = MultinomialNB()
 #clf = svm.NuSVC()
-clf = GaussianNB()
+#clf = GaussianNB()
 clf.fit(X.todense(), y)
 
 print clf.predict(_X.todense())
